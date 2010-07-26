f1dc88f704bfa922b0a175a6de821d6eca028bc9
stzschoppe <stzschoppe@b2d6ebbe-742c-0690-c5f8-0e04e6e12a4c>
2010-07-26 18:57:16 +0000

    Konstruktoren und Teile des Constrainchecks

---
+++ trunk/prdg/prdg/src/de/unibw/prdg/exceptions/ConstrainException.java
@@ -0,0 +1,26 @@
+package de.unibw.prdg.exceptions;
+
+public class ConstrainException extends Exception {
+
+	public ConstrainException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConstrainException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConstrainException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConstrainException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
