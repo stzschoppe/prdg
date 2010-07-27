eff229b3504e63ad14cefd1f7bf9999a0594cda7
stzschoppe <stzschoppe@b2d6ebbe-742c-0690-c5f8-0e04e6e12a4c>
2010-07-27 11:15:05 +0000

    GUI integriert, erste Funktionalität umgesetzt

---
+++ trunk/prdg/prdg/src/de/unibw/prdg/gui/GeneratorApp.java
@@ -0,0 +1,45 @@
+/*
+ * GeneratorApp.java
+ */
+
+package de.unibw.prdg.gui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class GeneratorApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new GeneratorView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+        this.getMainFrame().setResizable(false);
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of GeneratorApp
+     */
+    public static GeneratorApp getApplication() {
+        return Application.getInstance(GeneratorApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+        launch(GeneratorApp.class, args);
+    }
+}
